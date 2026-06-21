c67553d1a19dddb358c966f27d34b71cd8af10c4
rishabh <1244rishabh@gmail.com>
2026-06-21 21:57:04 +0530

    updates

--- playing_around.py
+++ playing_around.py
@@ -1,9 +1,8 @@
 import numpy as np
-import pandas as pd 
-import matplotlib.pyplot as plt 
+import pandas as pd
+import matplotlib.pyplot as plt
 
-
-# generating random data 
+# generating random data
 np.random.seed(42)
 
 n = 10000
@@ -13,58 +12,46 @@ bedrooms = np.random.randint(1, 6, n)
 
 noise = np.random.normal(0, 40000, n)
 
-price = (
-    500000
-    + 180 * size_sqft
-    + 15000 * bedrooms
-    + noise
-)
+price = 500000 + 180 * size_sqft + 15000 * bedrooms + noise
 
-df = pd.DataFrame({
-    "size_sqft": size_sqft,
-    "bedrooms": bedrooms,
-    "price": price
-})
+df = pd.DataFrame({"size_sqft": size_sqft, "bedrooms": bedrooms, "price": price})
 
 df.to_csv("data/housing_demo.csv", index=False)
 
-# testing for central limit theorem 
+# testing for central limit theorem
 data = pd.read_csv("data/housing_demo.csv")
 x = data["size_sqft"]
 
 print("actual mean : ", x.mean())
 
-sample_size =100
+sample_size = 100
 num_samples = 10000
 
 sample_means = []
 
 for _ in range(num_samples):
 
-    sample = x.sample(
-        n=sample_size,
-        replace=True
-    )
+    sample = x.sample(n=sample_size, replace=True)
 
     sample_means.append(sample.mean())
 
 
-true_SE = x.mean()/np.sqrt(num_samples)
-print("TRUE SAMPLE ERROR " ,true_SE)
+true_SE = x.mean() / np.sqrt(num_samples)
+print("TRUE SAMPLE ERROR ", true_SE)
 
 plt.style.use("dark_background")
 
 plt.figure(figsize=(8, 5))
-plt.hist(sample_means, bins=31, edgecolor='black', color='steelblue')
-plt.grid(axis='y', alpha=0.3)
+plt.hist(sample_means, bins=31, edgecolor="black", color="steelblue")
+plt.grid(axis="y", alpha=0.3)
 plt.xlabel("mean")
 plt.ylabel("freq")
 plt.show()
 
 
-# hypothesis testing 
+# hypothesis testing
 
-a = 2 
+a = 2
 b = 244
 
 sample_mean = x[a:b].mean()
@@ -72,12 +59,7 @@ std_dev = x[a:b].std()
 print(sample_mean)
 
 
-se = std_dev/np.sqrt(b-a)
-print("SE " , se)
+se = std_dev / np.sqrt(b - a)
+print("SE ", se)
 
-print("Z " , (sample_mean - x.mean()) / se )
-
-
-
-
-
+print("Z ", (sample_mean - x.mean()) / se)

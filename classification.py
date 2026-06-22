8e615ffdebc4436eaf35c7ef731e9a9b3dd2179c
rishabh <1244rishabh@gmail.com>
2026-06-22 08:09:01 +0530

    vis

--- classification.py
+++ classification.py
@@ -1,20 +1,27 @@
+# %%
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+# %%
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
+
+# %%
 data = pd.read_csv("./data/heart.csv").to_numpy()
 
 # need 1 more col containing 1s so we can have a scaling factor beta_n
-expected_output = data[:,-1]
+expected_output = data[:, -1]
 scaling_col = np.ones((data.shape[0], 1))
-data = data[:,:-1]
-data = np.column_stack((data,scaling_col))
+data = data[:, :-1]
+data = np.column_stack((data, scaling_col))
 
+# %%
 
-# geneate a parameter matrix 
+# geneate a parameter matrix
 # size = len(data[0])
 
 rng = np.random.default_rng()
@@ -25,27 +32,27 @@ print(data.shape)
 print(beta_param)
 print(beta_param.shape)
 
-# predicted value 
+# predicted value
 
 val = sigmoid(data @ beta_param)
 eps = 1e-15
 p = np.clip(val, eps, 1 - eps)
 
-# cross-entropy loss 
+# cross-entropy loss
 
+# %%
 log_likelihood = np.sum(
-    expected_output * np.log(p)
-    + (1 - expected_output) * np.log(1 - p)
+    expected_output * np.log(p) + (1 - expected_output) * np.log(1 - p)
 ) / len(data)
 
 print(log_likelihood)
 
-gradient = 1/len(data) * ( data.T @ (p - expected_output) )
+gradient = 1 / len(data) * (data.T @ (p - expected_output))
 learning_rate = 0.01
 beta_param -= learning_rate * gradient
-epochs =1025
+epochs = 1025
 
-for i in range(0,epochs):
+for i in range(0, epochs):
 
     z = data @ beta_param
 
@@ -63,11 +70,9 @@ p = sigmoid(z)
 p = np.clip(p, eps, 1 - eps)
 
 new_log_likelihood = np.mean(
-    expected_output * np.log(p)
-    + (1 - expected_output) * np.log(1 - p)
+    expected_output * np.log(p) + (1 - expected_output) * np.log(1 - p)
 )
 print(new_log_likelihood)
 
 
-
-
+# %%
--- linear_reg.py
+++ linear_reg.py
@@ -1,30 +1,30 @@
 import pandas as pd
-import numpy as np 
+import numpy as np
 import matplotlib.pyplot as plt
 
 data = pd.read_csv("data/house.csv")
 x = data["size_sqft"]
 y = data["price_usd"]
 
-# applying linear regression onto this 
+# applying linear regression onto this
 # Y = B1 * X  + B0
 # where B1 is the correlation coefficent working as the slope of the line (kinda obvious)
-# b1 = summation( ( xi - mean(x) ) * (yi - mean(y)) ) /  summation ( ( xi - mean(x))^2 )  
+# b1 = summation( ( xi - mean(x) ) * (yi - mean(y)) ) /  summation ( ( xi - mean(x))^2 )
 # b2 = mean(y) - b1 * mean(x)
 
-mean_x = np.mean(x);
+mean_x = np.mean(x)
 mean_y = np.mean(y)
-b1 = 0  
+b1 = 0
 b1_denom = 0
 
 for i in x:
     b1_denom += np.square(i - mean_x)
 
-for i in range(0,x.size):
-    b1 += (x[i] - mean_x) * (y[i]- mean_y)
+for i in range(0, x.size):
+    b1 += (x[i] - mean_x) * (y[i] - mean_y)
 
 b1 = b1 / b1_denom
-b2 = mean_y - b1 * mean_x;
+b2 = mean_y - b1 * mean_x
 
 y_pred = b2 + b1 * x
 
@@ -32,12 +32,12 @@ plt.style.use("dark_background")
 plt.scatter(x, y, label="Actual data")
 plt.plot(x, y_pred, color="red", label="Regression line")
 
+
+# %%
+
 plt.xlabel("size_sqft")
 plt.ylabel("price_usd")
 plt.legend()
 print(b1)
 print(b2)
 plt.show()
-
-
-
